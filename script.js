08319c485f64a478e6498719b68a85b289b479d4
vaibha987 <v2goyal95@gmail.com>
2024-09-22 04:15:03 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 //your JS code here. If required.
 const student={
 	name:"vaibhav",
-
+};
 	Object.prototype.getKeys=function(){
 		return Object.keys(this);
 	}
-	
-}
 console.log(student.getKeys());
